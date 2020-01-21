a5f5a69d499b182a9020304a0b7c5b1daf06bc32
Patty Arunyavikul <parunyavikul@gmail.com>
2020-01-21 12:55:24 -0500

    completed pwd command in bash.js

---
+++ bash.js
@@ -0,0 +1,16 @@
+// process.stdout.write('prompt > ');
+// process.stdin.on('data', (data) => {
+//   const cmd = data.toString().trim();
+//   process.stdout.write('You typed: ' + cmd);
+//   process.stdout.write('\nprompt >');
+// });
+
+process.stdout.write('prompt > ');
+
+process.stdin.on('data', (pwd) => {
+
+
+  process.stdout.write(process.cwd());
+
+
+})
