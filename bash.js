3118351238423dcc7bf581da9ff671a7b2f395ed
Patty Arunyavikul <parunyavikul@gmail.com>
2020-01-21 15:06:29 -0500

    Fixing the cat functionality

--- bash.js
+++ bash.js
@@ -1,9 +1,15 @@
-// process.stdout.write('prompt > ');
-// process.stdin.on('data', (data) => {
-//   const cmd = data.toString().trim();
-//   process.stdout.write('You typed: ' + cmd);
-//   process.stdout.write('\nprompt >');
-// });
+
+// const pwd = require('./pwd');
+// const ls = require('./ls');
+
+process.stdout.write('prompt > ');
+process.stdin.on('data', (data) => {
+  const arrCMD = data.toString().trim().split(' ');
+  const cmd = arrCMD[0];
+  const fileName = arrCMD[1];
+  require('./'+cmd);
+  process.stdout.write('\nprompt >');
+});
 
 // process.stdout.write('prompt > ');
 
@@ -15,5 +21,5 @@
 //   process.stdout.write('\nprompt >');
 // })
 
-const pwd = require('./pwd');
-pwd();
+
+
